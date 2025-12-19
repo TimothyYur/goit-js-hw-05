5a950ab8f956181f4a3d6542259e3a35a86c47b2
TimothyYur <timjurev@gmail.com>
2025-12-19 21:02:06 +0200

    starting up

---
+++ js/task-2.js
@@ -0,0 +1,41 @@
+function calcAverageCalories(days){
+  let caloriesArr = [];
+  let totalCalories = 0;
+  for(day of days){
+    caloriesArr.push(day.calories)
+  }
+  for(value of caloriesArr){
+    totalCalories += value
+  }
+  if(caloriesArr.length === 0){
+    return totalCalories
+  }
+  return totalCalories / caloriesArr.length
+}
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); // 2270
+
+console.log(
+  calcAverageCalories([])
+); // 0
